6c68fdd21a8a5c92c0d3c5f946effc955e8ce122
harryhei <harryx520@qq.com>
2020-06-23 22:13:23 +0800

    fix err

--- pipages/settings.py
+++ pipages/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Redis
 REDIS_HOST = config.get('redis', 'host')
