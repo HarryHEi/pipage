8184e3fbaf8937d0cd8ed8cdaf5b46a3740d8734
harryhei <harryx520@qq.com>
2020-07-18 18:20:58 +0800

    增加书封面

--- books/models.py
+++ books/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=64, verbose_name='书名')
+    cover = models.CharField(max_length=256, default='', blank=True)
 
     def __str__(self):
         return f'Book({self.title})'
